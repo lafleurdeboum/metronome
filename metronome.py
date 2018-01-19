efd95a0b9a4afab4b862d1693c65e72d391b7863
lafleurdeboum <lafleur@boum.org>
2018-01-19 17:21:42 +0100

    changed metronome.py's writing to object-oriented

--- metronome.py
+++ metronome.py
@@ -16,13 +16,8 @@ from mingus.core import value, chords
 from mingus.midi import fluidsynth
 
 
+default_bpm = 80
 SF2='african.sf2'
-if len(argv) == 2:
-    bpm = int(argv[1])
-elif len(argv) == 1:
-    bpm = 80
-else:
-    raise SystemExit(__doc__)
 
 
 def tellduration(starttime):
@@ -39,67 +34,65 @@ def tellduration(starttime):
     print(msg)
 
 
+class metronome():
+    def __init__(self):
+        silence = Bar()
+        silence.place_rest(1)
 
-# Chords
+        metronome_bar = Bar()
+        #metronome_bar.place_notes('D-4', value.dots(4))
+        metronome_bar.place_notes('D-5', 4)
+        metronome_bar.place_notes('D-4', 4)
+        metronome_bar.place_notes('D-4', 4)
+        metronome_bar.place_notes('D-4', 4)
+        # Use current_beat to check that the bar is full
+        if metronome_bar.current_beat != 1:
+            raise SystemExit('metronome_bar is not full !')
 
-basenote = 'C'
-#basenote.channel = 0
-i7 = NoteContainer(chords.I7(basenote))
-iv7 = NoteContainer(chords.IV7(basenote))
-v7 = NoteContainer(chords.V7(basenote))
+        drum = MidiInstrument()
+        #drum.name = "AFRICAN DRUM"
+        drum.name = drum.names[0]
+        #drum.instrument_nr = 1
 
-# Bars
+        self.metronome_track = Track(drum)
 
-silence = Bar()
-silence.place_rest(1)
+        for i in range(10):
+            # The mingus syntax is curious - this adds metronome_bar to metronome_track :
+            self.metronome_track + metronome_bar
 
-metronome_bar = Bar()
-#metronome_bar.place_notes('D-4', value.dots(4))
-metronome_bar.place_notes('D-5', 4)
-metronome_bar.place_notes('D-4', 4)
-metronome_bar.place_notes('D-4', 4)
-metronome_bar.place_notes('D-4', 4)
-# Use current_beat to check that the bar is full
-if metronome_bar.current_beat != 1:
-    raise SystemExit('metronome_bar is not full !')
-# Instruments
+        #if not fluidsynth.init(SF2):
+        if not fluidsynth.init(SF2, driver='alsa'):
+            raise SystemExit('Could not load ' + SF2)
 
-drum = MidiInstrument()
-#drum.name = "AFRICAN DRUM"
-drum.name = drum.names[0]
-#drum.instrument_nr = 1
+        fluidsynth.set_instrument(0, 0)
+        fluidsynth.main_volume(0, 127)
 
-# Tracks
+        # Breathe a (tenth of) second, toss it all
+        sleep(0.1)
 
-metronome_track = Track(drum)
-
-for i in range(10):
-    # The mingus syntax is curious - this adds metronome_bar to metronome_track :
-    metronome_track + metronome_bar
-
-#if not fluidsynth.init(SF2):
-if not fluidsynth.init(SF2, driver='alsa'):
-    raise SystemExit('Could not load ' + SF2)
-
-fluidsynth.set_instrument(0, 0)
-fluidsynth.main_volume(0, 127)
-
-# Breathe a (tenth of) second, toss it all
-sleep(0.1)
-
-print('press a key to start')
-key = getch()
-while True:
-    try:
-        starttime = time()
-        while True:
-            fluidsynth.play_Track(metronome_track, bpm=bpm)
-    except KeyboardInterrupt:
-        tellduration(starttime)
+    def run(self, bpm):
+        print('press a key to start')
         key = getch()
-        if key == 'q':
-            raise SystemExit
-        else:
-            continue
+        while True:
+            try:
+                starttime = time()
+                while True:
+                    fluidsynth.play_Track(self.metronome_track, bpm=bpm)
+            except KeyboardInterrupt:
+                tellduration(starttime)
+                key = getch()
+                if key == 'q':
+                    raise SystemExit
+                else:
+                    continue
 
 
+if __name__ == '__main__':
+    if len(argv) == 2:
+        bpm = int(argv[1])
+    elif len(argv) == 1:
+        bpm = default_bpm
+    else:
+        raise SystemExit(__doc__)
+    this = metronome()
+    this.run(bpm)
